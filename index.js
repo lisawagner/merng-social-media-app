2b11d1c1fec622a7804f3cfba6c85fdbf8ab11c7
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-28 12:36:05 -0700

    created server

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+// posts query typeDefs moved to graphql folder
+// resolvers moved
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
